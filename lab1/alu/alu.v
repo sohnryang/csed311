298b5bf5ec4366396438f8cc0f76dff3fb6d7917
Aplace <aplace030927@gmail.com>
2024-02-28 00:16:19 +0900

    feat(alu): alu implement and testbenches

--- lab1/alu/alu.v
+++ lab1/alu/alu.v
@@ -1,4 +1,6 @@
-module ALU #(parameter data_width = 16) (
+`include "alu_func.v"
+
+module alu #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
 	input [3 : 0] FuncCode,
@@ -21,6 +23,74 @@ end
 /*
 	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
 */
+	always begin
+		case (FuncCode)
+			`FUNC_ADD: begin
+				assign C = A + B;
+				assign OverflowFlag = ~(A[data_width - 1] ^ B[data_width - 1]) & (A[data_width - 1] ^ C[data_width - 1]);
+			end
+			`FUNC_SUB: begin
+				assign C = A - B;
+				assign OverflowFlag = ~(A[data_width - 1] ^ B[data_width - 1]) & (A[data_width - 1] ^ C[data_width - 1]);
+			end
+			`FUNC_ID: begin
+				assign C = A;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_NOT: begin
+				assign C = ~A;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_AND: begin
+				assign C = A & B;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_OR: begin
+				assign C = A | B;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_NAND: begin
+				assign C = ~(A & B);
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_NOR: begin
+				assign C = ~(A | B);
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_XOR: begin
+				assign C = A ^ B;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_XNOR: begin
+				assign C = ~(A ^ B);
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_LLS: begin
+				assign C = A << 1;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_LRS: begin
+				assign C = A >> 1;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_ALS: begin
+				assign C = A <<< 1;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_ARS: begin
+				assign C = A >>> 1;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_TCP: begin
+				assign C = ~A + 1;
+				assign OverflowFlag = 1'b0;
+			end
+			`FUNC_ZERO: begin
+				assign C = 16'b0000_0000_0000_0000;
+				assign OverflowFlag = 1'b0;
+			end
+		endcase
+	end
 
 endmodule
 
--- lab1/alu/tb_alu.cpp
+++ lab1/alu/tb_alu.cpp
@@ -6,12 +6,9 @@
 #include "Valu.h"
 #include "Valu___024root.h"
 
-
 #define MAX_SIM_TIME 40
 vluint64_t sim_time = 0;
 vluint64_t posedge_cnt = 0;
-
-
 
 // `define FUNC_ADD    4'b0000
 // `define FUNC_SUB    4'b0001
@@ -30,8 +27,8 @@ vluint64_t posedge_cnt = 0;
 // `define FUNC_TCP    4'b1110
 // `define FUNC_ZERO   4'b1111
 
-
-int main(int argc, char** argv, char** env) {
+int main(int argc, char **argv, char **env)
+{
     Verilated::commandArgs(argc, argv);
     Valu *dut = new Valu;
 
@@ -41,231 +38,230 @@ int main(int argc, char** argv, char** env) {
     m_trace->open("waveform.vcd");
 
     int sum = 0;
-    
+
     dut->A = 0;
     dut->B = 0;
     dut->FuncCode = 0;
     dut->eval();
 
-    while (sim_time < MAX_SIM_TIME) {
-        if(sim_time >= 0 && sim_time < 2)
+    while (sim_time < MAX_SIM_TIME)
+    {
+        if (sim_time >= 0 && sim_time < 2)
         {
-            dut -> A = 1;
-            dut -> B = 1;
-            dut -> FuncCode = 0;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 1 && (dut -> C == dut -> A + dut -> B))
+            dut->A = 1;
+            dut->B = 1;
+            dut->FuncCode = 0;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 1 && (dut->C == dut->A + dut->B))
             {
                 printf("Test 1 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 2 && sim_time < 4)
+        if (sim_time >= 2 && sim_time < 4)
         {
-            dut -> A = 1;
-            dut -> B = 1;
-            dut -> FuncCode = 0;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 3 && (dut -> C == dut -> A + dut -> B))
+            dut->A = 1;
+            dut->B = 1;
+            dut->FuncCode = 0;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 3 && (dut->C == dut->A + dut->B))
             {
                 printf("Test 2 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 4 && sim_time < 6)
+        if (sim_time >= 4 && sim_time < 6)
         {
-            dut -> A = 1;
-            dut -> B = 1;
-            dut -> FuncCode = 1;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 5 && (dut -> C == dut -> A - dut -> B))
+            dut->A = 1;
+            dut->B = 1;
+            dut->FuncCode = 1;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 5 && (dut->C == dut->A - dut->B))
             {
                 printf("Test 3 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 6 && sim_time < 8)
+        if (sim_time >= 6 && sim_time < 8)
         {
-            dut -> A = 1;
-            dut -> B = 1;
-            dut -> FuncCode = 2;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 7 && (dut -> C == dut -> A))
+            dut->A = 1;
+            dut->B = 1;
+            dut->FuncCode = 2;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 7 && (dut->C == dut->A))
             {
                 printf("Test 4 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 8 && sim_time < 10)
+        if (sim_time >= 8 && sim_time < 10)
         {
-            dut -> A = 1;
-            dut -> B = 1;
-            dut -> FuncCode = 3;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 9 && (dut -> C == (uint16_t)~(dut -> A)))
+            dut->A = 1;
+            dut->B = 1;
+            dut->FuncCode = 3;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 9 && (dut->C == (uint16_t) ~(dut->A)))
             {
                 printf("Test 5 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 10 && sim_time < 12)
+        if (sim_time >= 10 && sim_time < 12)
         {
-            dut -> A = 11;
-            dut -> B = 5;
-            dut -> FuncCode = 4;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 11 && (dut -> C == (u_int16_t)(dut -> A & dut -> B)))
+            dut->A = 11;
+            dut->B = 5;
+            dut->FuncCode = 4;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 11 && (dut->C == (u_int16_t)(dut->A & dut->B)))
             {
                 printf("Test 6 Passed\n");
                 sum++;
             }
         }
-        
-        if(sim_time >= 12 && sim_time < 14)
+
+        if (sim_time >= 12 && sim_time < 14)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 5;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 13 && (dut -> C == (u_int16_t)(dut -> A | dut -> B)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 5;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 13 && (dut->C == (u_int16_t)(dut->A | dut->B)))
             {
                 printf("Test 7 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 14 && sim_time < 16)
+        if (sim_time >= 14 && sim_time < 16)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 6;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 15 && (dut -> C == (u_int16_t)~(dut -> A & dut -> B)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 6;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 15 && (dut->C == (u_int16_t) ~(dut->A & dut->B)))
             {
                 printf("Test 8 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 16 && sim_time < 18)
+        if (sim_time >= 16 && sim_time < 18)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 7;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 17 && (dut -> C == (u_int16_t)~(dut -> A | dut -> B)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 7;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 17 && (dut->C == (u_int16_t) ~(dut->A | dut->B)))
             {
                 printf("Test 9 Passed\n");
                 sum++;
             }
         }
-   
 
-        if(sim_time >= 18 && sim_time < 20)
+        if (sim_time >= 18 && sim_time < 20)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 8;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 19 && (dut -> C == (u_int16_t)(dut -> A ^ dut -> B)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 8;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 19 && (dut->C == (u_int16_t)(dut->A ^ dut->B)))
             {
                 printf("Test 10 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 20 && sim_time < 22)
+        if (sim_time >= 20 && sim_time < 22)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 9;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 21 && (dut -> C == (u_int16_t)~(dut -> A ^ dut -> B)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 9;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 21 && (dut->C == (u_int16_t) ~(dut->A ^ dut->B)))
             {
                 printf("Test xnor 11 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 22 && sim_time < 24)
+        if (sim_time >= 22 && sim_time < 24)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 10;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 23 && (dut -> C == (u_int16_t)(dut -> A << 1)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 10;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 23 && (dut->C == (u_int16_t)(dut->A << 1)))
             {
                 printf("Test 12 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 24 && sim_time < 26)
+        if (sim_time >= 24 && sim_time < 26)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 11;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 25 && (dut -> C == (u_int16_t)(dut -> A >> 1)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 11;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 25 && (dut->C == (u_int16_t)(dut->A >> 1)))
             {
                 printf("Test 13 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 26 && sim_time < 28)
+        if (sim_time >= 26 && sim_time < 28)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 12;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 27 && (dut -> C == (u_int16_t)(dut -> A << 1)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 12;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 27 && (dut->C == (u_int16_t)(dut->A << 1)))
             {
                 printf("Test 14 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 28 && sim_time < 30)
+        if (sim_time >= 28 && sim_time < 30)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 13;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if (sim_time == 29 && (dut -> C == (u_int16_t)(dut -> A >> 1)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 13;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if (sim_time == 29 && (dut->C == (u_int16_t)(dut->A >> 1)))
             {
                 printf("Test 15 Passed\n");
                 sum++;
             }
         }
 
-        if(sim_time >= 30 && sim_time < 32)
+        if (sim_time >= 30 && sim_time < 32)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 14;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
-            if ((sim_time == 31) && (dut -> C == (u_int16_t)~(dut -> A + 1)))
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 14;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
+            if ((sim_time == 31) && (dut->C == (u_int16_t) ~(dut->A + 1)))
             {
                 printf("Test 16 Passed\n");
                 sum++;
             }
-
         }
 
-        if(sim_time >= 32 && sim_time < 34)
+        if (sim_time >= 32 && sim_time < 34)
         {
-            dut -> A = 11;  // 1011
-            dut -> B = 5;   // 0101 
-            dut -> FuncCode = 15;
-            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n",sim_time, dut -> A, dut -> B, dut -> FuncCode, dut -> C);
+            dut->A = 11; // 1011
+            dut->B = 5;  // 0101
+            dut->FuncCode = 15;
+            printf("sim_time : %d  A = %d, B = %d, FuncCode = %d, Result = %d\n", sim_time, dut->A, dut->B, dut->FuncCode, dut->C);
 
-            if ((sim_time == 33) && (dut -> C == 0))
+            if ((sim_time == 33) && (dut->C == 0))
             {
                 printf("Test 17 Passed\n");
                 sum++;
@@ -274,11 +270,11 @@ int main(int argc, char** argv, char** env) {
 
         if (sim_time >= 34 && sim_time < 36)
         {
-            dut -> A = 0xFFFF;  // 16'b1111111111111111
-            dut -> B = 0x8001; // 16'b1000000000000001 
-            dut -> FuncCode = 0;
+            dut->A = 0xFFFF; // 16'b1111111111111111
+            dut->B = 0x8001; // 16'b1000000000000001
+            dut->FuncCode = 0;
             printf(" overflow check\n ");
-            if (sim_time == 35 && (dut ->OverflowFlag > 0))
+            if (sim_time == 35 && (dut->OverflowFlag > 0))
             {
                 printf("sim_time : %llu overflow!!\n", sim_time);
             }
@@ -286,39 +282,35 @@ int main(int argc, char** argv, char** env) {
 
         if (sim_time >= 34 && sim_time < 36)
         {
-            dut -> A = 0xFFFF;  // 16'b1111111111111111
-            dut -> B = 0x8001; // 16'b1000000000000001 
-            dut -> FuncCode = 0;
+            dut->A = 0xFFFF; // 16'b1111111111111111
+            dut->B = 0x8001; // 16'b1000000000000001
+            dut->FuncCode = 0;
             printf(" overflow check\n ");
-            if (sim_time == 35 && (dut ->OverflowFlag > 0))
+            if (sim_time == 35 && (dut->OverflowFlag > 0))
             {
                 printf("sim_time : %llu  + overflow!!\n", sim_time);
             }
         }
-        
+
         if (sim_time >= 36 && sim_time < 38)
         {
-            dut -> A = -65535;  // 16'b1111111111111111
-            dut -> B = -1; // 16'b1000000000000001 
-            dut -> FuncCode = 0;
+            dut->A = -65535; // 16'b1111111111111111
+            dut->B = -1;     // 16'b1000000000000001
+            dut->FuncCode = 0;
             printf(" overflow check\n ");
-            if (sim_time == 37 && (dut ->OverflowFlag > 0))
+            if (sim_time == 37 && (dut->OverflowFlag > 0))
             {
                 printf("sim_time : %llu  - overflow!!\n", sim_time);
             }
         }
-
 
         if (sum == 17)
         {
             printf(" sim_time : %llu All tests passed\n", sim_time);
         }
 
-        
-
-
         dut->eval();
-        
+
         m_trace->dump(sim_time);
         sim_time++;
     }
