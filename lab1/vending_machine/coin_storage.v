561fae90a85ca98810baea360b764096acb7bac5
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-05 01:05:07 +0900

    refactor(lab1): handle return delay in `coin_storage` for accurate timing

--- lab1/vending_machine/coin_storage.v
+++ lab1/vending_machine/coin_storage.v
@@ -8,6 +8,7 @@ module coin_storage (
     coin_input,
     buy_price_input,
     trigger_return,
+    timeout,
 
     coin_value,
     return_coins
@@ -19,6 +20,7 @@ module coin_storage (
   input [`kNumCoins-1:0] coin_input;
   input [31:0] buy_price_input;
   input trigger_return;
+  input timeout;
 
   output reg [31:0] coin_value;
   output reg [`kNumCoins-1:0] return_coins;
@@ -46,12 +48,24 @@ module coin_storage (
 
   reg [31:0] coin_value_next;
   reg [`kNumCoins-1:0] return_coins_next;
+  reg [1:0] remaining_time, remaining_time_next;
+  reg timer_started, timer_started_next;
   always @(*) begin
-    if (trigger_return) begin
-      coin_value_next   = coin_value - return_value_mod_output;
+    if (timeout || (trigger_return && remaining_time == 2'b0)) begin
+      coin_value_next = coin_value - return_value_mod_output;
       return_coins_next = return_coins_mod_output;
-    end else begin
+      timer_started_next = 0;
+      remaining_time_next = 2'b0;
+    end else if (trigger_return) begin
+      coin_value_next   = coin_value;
       return_coins_next = `kNumCoins'b0;
+      if (timer_started) remaining_time_next = remaining_time - 2'b1;
+      else remaining_time_next = 2'd`kTriggerWait;
+      timer_started_next = 1;
+    end else begin
+      return_coins_next   = `kNumCoins'b0;
+      timer_started_next  = 0;
+      remaining_time_next = 2'd`kTriggerWait;
       if (buy_price_input != 32'b0) coin_value_next = coin_value - buy_price_input;
       else coin_value_next = coin_value + input_value;
     end
@@ -59,11 +73,15 @@ module coin_storage (
 
   always @(posedge clk) begin
     if (!reset_n) begin
-      coin_value   <= 32'b0;
+      coin_value <= 32'b0;
       return_coins <= `kNumCoins'b0;
+      remaining_time <= 2'b0;
+      timer_started <= 0;
     end else begin
-      coin_value   <= coin_value_next;
+      coin_value <= coin_value_next;
       return_coins <= return_coins_next;
+      remaining_time <= remaining_time_next;
+      timer_started <= timer_started_next;
     end
   end
 endmodule
