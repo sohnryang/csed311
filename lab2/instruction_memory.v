44512607c41539fe53f8efeafda5a974bd03b9aa
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-12 19:34:16 +0900

    chore(lab2): format sources

--- lab2/instruction_memory.v
+++ lab2/instruction_memory.v
@@ -1,7 +1,11 @@
-module instruction_memory #(parameter MEM_DEPTH = 1024) (input reset,
-                                                         input clk,
-                                                         input [31:0] addr,   // address of the instruction memory
-                                                         output [31:0] dout); // instruction at addr
+module instruction_memory #(
+    parameter MEM_DEPTH = 1024
+) (
+    input reset,
+    input clk,
+    input [31:0] addr,  // address of the instruction memory
+    output [31:0] dout
+);  // instruction at addr
   integer i;
   // Instruction memory
   reg [31:0] mem[0:MEM_DEPTH - 1];
@@ -9,10 +13,7 @@ module instruction_memory #(parameter MEM_DEPTH = 1024) (input reset,
   wire [9:0] imem_addr;
   assign imem_addr = addr[11:2];
   // Do not touch or use this wire
-  wire _unused_ok = &{1'b0,
-                  addr[31:12],
-                  addr[1:0],
-                  1'b0};
+  wire _unused_ok = &{1'b0, addr[31:12], addr[1:0], 1'b0};
 
   // TODO
   // Asynchronously read instruction from the memory 
@@ -22,11 +23,11 @@ module instruction_memory #(parameter MEM_DEPTH = 1024) (input reset,
   always @(posedge clk) begin
     if (reset) begin
       for (i = 0; i < MEM_DEPTH; i = i + 1)
-        // DO NOT TOUCH COMMENT BELOW
-        /* verilator lint_off BLKSEQ */
-        mem[i] = 32'b0;
-        /* verilator lint_on BLKSEQ */
-        // DO NOT TOUCH COMMENT ABOVE
+      // DO NOT TOUCH COMMENT BELOW
+      /* verilator lint_off BLKSEQ */
+      mem[i] = 32'b0;
+      /* verilator lint_on BLKSEQ */
+      // DO NOT TOUCH COMMENT ABOVE
 
       // Provide path of the file including instructions with binary format
       $readmemh("/path/to/binary_format/file", mem);
@@ -34,5 +35,3 @@ module instruction_memory #(parameter MEM_DEPTH = 1024) (input reset,
   end
 
 endmodule
-
-
