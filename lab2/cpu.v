d7e2b279a6a58f498f8c17b5ca39780283a3597e
Aplace <aplace030927@gmail.com>
2024-03-18 18:00:06 +0900

    fix(lab2): corrected unmatching wire name, commented

--- lab2/cpu.v
+++ lab2/cpu.v
@@ -41,7 +41,7 @@ module cpu (
   wire [31:0] current_pc_address;
   wire [31:0] next_pc_address;
   wire [31:0] adjacent_pc_address;
-  wire [31:0] adder_offset_pc_address;
+  wire [31:0] added_offset_pc_address;
   wire [31:0] branch_determined_pc_address;
 
   /***** Register declarations *****/
@@ -51,15 +51,15 @@ module cpu (
   pc pc (
       .reset     (reset),  // input (Use reset to initialize PC. Initial value must be 0)
       .clk       (clk),  // input
-      .next_pc   (next_pc_address),  // input
-      .current_pc(current_pc_address)   // output
+      .next_pc   (next_pc_address),  // MUX_ALU_ADDRESS_SELECT.MUX_OUT -> 
+      .current_pc(current_pc_address)   // -> IMEM.ADDR, -> ADDER_ADJACENT_PC_ADDRESS.MUX_IN_0 ->, ADDER_OFFSET_PC_ADDRESS.MUX_IN_0
   );
 
   // ---------- Instruction Memory ----------
   instruction_memory imem(
     .reset(reset),   // input
     .clk(clk),     // input
-    .addr(current_pc_address),    // input
+    .addr(current_pc_address),    // PC.CURRENT_PC ->
     .dout(instruction)     // output
   );
 
@@ -70,10 +70,10 @@ module cpu (
     .rs1 (instruction[19:15]),          // input
     .rs2 (instruction[24:20]),          // input
     .rd (instruction[11:7]),           // input
-    .rd_din (register_write_data_out),       // input
-    .write_enable (control_unit_output[`CONTROL_REG_WRITE]), // input
-    .rs1_dout (reg_file_read_dout1),     // output
-    .rs2_dout (reg_file_read_dout2),     // output
+    .rd_din (register_write_data),       // MUX_REGISTER_WRITE_DATA_SELECT.MUX_OUT ->
+    .write_enable (control_unit_output[`CONTROL_REG_WRITE]), // CTRL_UNIT -> 
+    .rs1_dout (reg_file_read_dout1),     // -> ALU.ALU_IN_1
+    .rs2_dout (reg_file_read_dout2),     // -> DMEM.DIN, -> MUX_ALU_IN_2_SELECT.MUX_IN_0
     .print_reg (print_reg)  //DO NOT TOUCH THIS
   );
 
@@ -81,94 +81,94 @@ module cpu (
   // ---------- Control Unit ----------
   control_unit ctrl_unit (
     .part_of_inst(instruction[6:0]),  // input
-    .is_jal(control_unit_output[`CONTROL_JAL]),        // output
-    .is_jalr(control_unit_output[`CONTROL_JALR]),       // output
-    .branch(control_unit_output[`CONTROL_BRANCH]),        // output
-    .mem_read(control_unit_output[`CONTROL_MEM_READ]),      // output
-    .mem_to_reg(control_unit_output[`CONTROL_MEM_TO_REG]),    // output
-    .mem_write(control_unit_output[`CONTROL_MEM_WRITE]),     // output
-    .alu_src(control_unit_output[`CONTROL_ALU_SRC]),       // output
-    .write_enable(control_unit_output[`CONTROL_REG_WRITE]),  // output
-    .pc_to_reg(control_unit_output[`CONTROL_PC_TO_REG]),     // output
+    .is_jal(control_unit_output[`CONTROL_JAL]),        // -> MUX_BRANCH_SELECT.SEL
+    .is_jalr(control_unit_output[`CONTROL_JALR]),       // -> MUX_ALU_ADDRESS_SELECT.SEL
+    .branch(control_unit_output[`CONTROL_BRANCH]),        // -> MUX_BRANCH_SELECT.SEL
+    .mem_read(control_unit_output[`CONTROL_MEM_READ]),      // -> DMEM.MEM_READ
+    .mem_to_reg(control_unit_output[`CONTROL_MEM_TO_REG]),    // -> DMEM_OUT_SELECT.SEL
+    .mem_write(control_unit_output[`CONTROL_MEM_WRITE]),     // -> DMEM.MEM_WRITE
+    .alu_src(control_unit_output[`CONTROL_ALU_SRC]),       // -> MUX_ALU_IN2_SELECT.SEL
+    .write_enable(control_unit_output[`CONTROL_REG_WRITE]),  // -> REG_FILE.WRITE_ENABLE
+    .pc_to_reg(control_unit_output[`CONTROL_PC_TO_REG]),     // -> MUX_REGISTER_WRITE_DATA_SELECT.SEL
     .is_ecall(control_unit_output[`CONTROL_IS_ECALL])       // output (ecall inst)
   );
 
   // ---------- Immediate Generator ----------
   immediate_generator imm_gen(
     .part_of_inst(instruction),  // input
-    .imm_gen_out(imm_gen_ouput)    // output
+    .imm_gen_out(imm_gen_ouput)    // -> ADDER_OFFSET_PC_ADDRESS.ADDER_IN_1,  MUX_ALU_IN2_SELECT.MUX_IN_1
   );
 
   // ---------- ALU Control Unit ----------
   alu_control_unit alu_ctrl_unit (
     .part_of_inst({inst[30], instruction[14:12], instruction[6:0]}),  // input
-    .alu_op(alu_operation)         // output
+    .alu_op(alu_operation)         // -> ALU.ALU_OP
   );
 
   // ---------- ALU ----------
   alu alu (
-    .alu_op(alu_operation),      // input
-    .alu_in_1(reg_file_read_dout1),    // input  
-    .alu_in_2(alu_in2_input),    // input
-    .alu_result(alu_output),  // output
-    .alu_bcond(alu_bcond_output)    // output
+    .alu_op(alu_operation),      // ALU_CTRL_UNIT.ALU_OP ->
+    .alu_in_1(reg_file_read_dout1),    // REG_FILE.RS1_DOUT -> 
+    .alu_in_2(alu_in2_input),    // MUX_ALU_IN2_SELECT.MUX_OUT -> 
+    .alu_result(alu_output),  // -> MUX_ALU_ADDRESS_SELECT.MUX_IN_1, -> DMEM.ADDR, -> MUX_DMEM_OUT_SELECT.MUX_IN_0
+    .alu_bcond(alu_bcond_output)    // -> MUX_BRANCH_SELECT.SEL
   );
 
   // ---------- Data Memory ----------
   data_memory dmem(
     .reset (reset),      // input
     .clk (clk),        // input
-    .addr (alu_result),       // input
-    .din (reg_file_read_dout2),        // input
-    .mem_read (control_unit_output[`CONTROL_MEM_READ]),   // input
-    .mem_write (control_unit_output[`CONTROL_MEM_WRITE]),  // input
-    .dout (dmem_output)        // output
+    .addr (alu_result),       // ALU.ALU_RESULT -> 
+    .din (reg_file_read_dout2),   // REG_FILE.RS2_DOUT -> 
+    .mem_read (control_unit_output[`CONTROL_MEM_READ]),   // CTRL_UNIT -> 
+    .mem_write (control_unit_output[`CONTROL_MEM_WRITE]),  // CTRL_UNIT ->
+    .dout (dmem_output)        // -> MUX_DMEM_OUT_SELECT.MUX_IN_1
   );
 
   mux32bit mux_register_write_data_select(
-    .mux_in_0(mux_dmem_output),  // Data memeory out
-    .mux_in_1(adjacent_pc_address),  // PC + 4
-    .sel(control_unit_output[`CONTROL_PC_TO_REG]),
-    .mux_out(register_write_data)  // to reg_file.rd_din
+    .mux_in_0(mux_dmem_output),  // MUX_DMEM_OUT.MUX_OUT ->
+    .mux_in_1(adjacent_pc_address),  // ADDER_ADJACENT_PC_ADDRESS.ADDER_OUT ->
+    .sel(control_unit_output[`CONTROL_PC_TO_REG]), // CTRL_UNIT ->
+    .mux_out(register_write_data)  // -> REG_FILE.RD_DIN
   );
 
   mux32bit mux_alu_in_2_select(
-    .mux_in_0(reg_file_read_dout2),  // reg_file.rs2_dout
-    .mux_in_1(imm_gen_ouput),  // imm_gen.imm_gen_out
-    .sel(control_unit_output[`CONTROL_ALU_SRC]),
-    .mux_out(alu_in2_input)  // to alu.alu_in_2
+    .mux_in_0(reg_file_read_dout2),  // REG_FILE.RS2_DOUT -> 
+    .mux_in_1(imm_gen_ouput),  // IMM_GEN.IMM_GEN_OUT -> 
+    .sel(control_unit_output[`CONTROL_ALU_SRC]),  // CTRL_UNIT -> 
+    .mux_out(alu_in2_input)  // -> ALU.ALU_IN_2
   );
 
   mux32bit mux_dmem_out_select(
-    .mux_in_0(alu_result),
-    .mux_in_1(dmem_output),
-    .sel(control_unit_output[`CONTROL_MEM_TO_REG]),
-    .mux_out(mux_dmem_output)
+    .mux_in_0(alu_result),  // ALU.ALU_RESULT -> 
+    .mux_in_1(dmem_output), // DMEM.DOUT -> 
+    .sel(control_unit_output[`CONTROL_MEM_TO_REG]), // CTRL_UNIT ->
+    .mux_out(mux_dmem_output) // -> MUX_REGISTER_WRITE_DATA_SELECT.MUX_IN_0
   );
 
   adder32bit adder_adjacent_pc_address(
-    .adder_in_0(current_pc_address),
+    .adder_in_0(current_pc_address), // PC.CURRENT_PC ->
     .adder_in_1(32'h00000004),
-    .adder_out(adjacent_pc_address)
+    .adder_out(adjacent_pc_address) // -> MUX_BRANCH_SELECT.MUX_IN_0, -> MUX_REGISTER_FILE_SELECT
   );
 
   adder32bit adder_offset_pc_address(
-    .adder_in_0(current_pc_address),
-    .adder_in_1(imm_gen_ouput),
-    .adder_out(adder_offset_pc_address)
+    .adder_in_0(current_pc_address), // PC.CURRENT_PC -> 
+    .adder_in_1(imm_gen_ouput), // IMM_GEN.IMM_GEN_OUT ->
+    .adder_out(added_offset_pc_address) // -> MUX_BRANCH_SELECT.MUX_IN_1
   );
 
   mux32bit mux_branch_select(
-    .mux_in_0(adjacent_pc_address),
-    .mux_in_1(adder_offset_pc_address),
-    .sel(control_unit_output[`CONTROL_JAL] | (control_unit_output[`CONTROL_BRANCH] & alu_bcond_output)),
-    .mux_out(branch_determined_pc_address)
+    .mux_in_0(adjacent_pc_address), // ADDER_ADJACENT_PC_ADDRESS.ADDER_OUT ->
+    .mux_in_1(added_offset_pc_address), // ADDER_OFFSET_PC_ADDRESS.ADDER_OUT ->
+    .sel(control_unit_output[`CONTROL_JAL] | (control_unit_output[`CONTROL_BRANCH] & alu_bcond_output)), // CTRL_UNIT -> , CTRL_UNIT ->, ALU.ALU_BCOND
+    .mux_out(branch_determined_pc_address)  // -> MUX_ALU_ADDRESS_SELECT.MUX_IN_0
   );
 
   mux32bit mux_alu_address_select(
-    .mux_in_0(branch_determined_pc_address),
-    .mux_in_1(alu_output),
-    .sel(control_unit_output[`CONTROL_JALR]),
-    .mux_out(current_pc_address)
+    .mux_in_0(branch_determined_pc_address),  // MUX_BRANCH_SELECT.MUX_OUT -> 
+    .mux_in_1(alu_output), // ALU.ALU_RESULT ->
+    .sel(control_unit_output[`CONTROL_JALR]), // CTRL_UNIT ->
+    .mux_out(next_pc_address) // -> PC.NEXT_PC
   )
 endmodule
