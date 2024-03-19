a4a90c3c058270ff3b348e2c22ad6910338e2e9d
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-19 20:01:11 +0900

    feat(lab2.alu): implement GE and GEU operations

--- lab2/alu.v
+++ lab2/alu.v
@@ -5,47 +5,26 @@ module alu (
     input [31:0] alu_in_1,
     input [31:0] alu_in_2,
 
-    output [31:0] alu_result
+    output reg [31:0] alu_result
 );
 
-  always begin
+  always @(*) begin
     case (alu_op)
-      `ALU_ADD: begin
-        assign alu_result = alu_in_1 + alu_in_2;
-      end
-      `ALU_SUB: begin
-        assign alu_result = alu_in_1 - alu_in_2;
-      end
-      `ALU_SLL: begin
-        assign alu_result = alu_in_1 << alu_in_2;
-      end
-      `ALU_SLT: begin
-        assign alu_result = {31'b0, $signed(alu_in_1) < $signed(alu_in_2)};
-      end
-      `ALU_SLTU: begin
-        assign alu_result = {31'b0, alu_in_1 < alu_in_2};
-      end
-      `ALU_XOR: begin
-        assign alu_result = alu_in_1 ^ alu_in_2;
-      end
-      `ALU_SRL: begin
-        assign alu_result = alu_in_1 >> alu_in_2;
-      end
-      `ALU_SRA: begin
-        assign alu_result = $signed(alu_in_1) >>> alu_in_2;
-      end
-      `ALU_OR: begin
-        assign alu_result = alu_in_1 | alu_in_2;
-      end
-      `ALU_AND: begin
-        assign alu_result = alu_in_1 & alu_in_2;
-      end
-      `ALU_EQ: begin
-        assign alu_result = {31'b0, alu_in_1 == alu_in_2};
-      end
-      `ALU_NE: begin
-        assign alu_result = {31'b0, alu_in_1 != alu_in_2};
-      end
+      `ALU_ADD:  alu_result = alu_in_1 + alu_in_2;
+      `ALU_SUB:  alu_result = alu_in_1 - alu_in_2;
+      `ALU_SLL:  alu_result = alu_in_1 << alu_in_2;
+      `ALU_SLT:  alu_result = {31'b0, $signed(alu_in_1) < $signed(alu_in_2)};
+      `ALU_SLTU: alu_result = {31'b0, alu_in_1 < alu_in_2};
+      `ALU_XOR:  alu_result = alu_in_1 ^ alu_in_2;
+      `ALU_SRL:  alu_result = alu_in_1 >> alu_in_2;
+      `ALU_SRA:  alu_result = $signed(alu_in_1) >>> alu_in_2;
+      `ALU_OR:   alu_result = alu_in_1 | alu_in_2;
+      `ALU_AND:  alu_result = alu_in_1 & alu_in_2;
+      `ALU_EQ:   alu_result = {31'b0, alu_in_1 == alu_in_2};
+      `ALU_NE:   alu_result = {31'b0, alu_in_1 != alu_in_2};
+      `ALU_GE:   alu_result = {31'b0, $signed(alu_in_1) >= $signed(alu_in_2)};
+      `ALU_GEU:  alu_result = {31'b0, alu_in_1 >= alu_in_2};
+      default:   alu_result = 32'hffffffff;
     endcase
   end
 endmodule
