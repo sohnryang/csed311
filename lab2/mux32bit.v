1ba9ba7c43dbba67a8eb09a027400a60edec33bc
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-19 20:29:37 +0900

    fix(lab2.mux32bit): use unambiguous code
    For some reason, verilator infers latch here

--- lab2/mux32bit.v
+++ lab2/mux32bit.v
@@ -6,10 +6,7 @@ module mux32bit (
     output reg [31:0] mux_out
 );
   always @(*) begin
-    if (sel == 1'b0) begin
-      mux_out = mux_in_0;
-    end else if (sel == 1'b1) begin
-      mux_out = mux_in_1;
-    end
+    if (sel) mux_out = mux_in_0;
+    else mux_out = mux_in_1;
   end
 endmodule
