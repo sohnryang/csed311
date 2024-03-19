66a13a100538dd8dace01bdde357804f285b6479
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-19 19:42:38 +0900

    fix(lab2.adder): fix syntax error

--- lab2/adder.v
+++ lab2/adder.v
@@ -1,11 +1,9 @@
-module adder32bit(
-    input [31:0] adder_in_0,
-    input [31:0] adder_in_1,
+module adder32bit (
+    input  [31:0] adder_in_0,
+    input  [31:0] adder_in_1,
     output [31:0] adder_out
 );
-
-  always begin
+  always @(*) begin
     adder_out = adder_in_0 + adder_in_1;
   end
-
 endmodule
