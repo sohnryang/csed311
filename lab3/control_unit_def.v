1554a6f98efa62a8dadbf0d9d93aa6079bd8660c
Ryang Sohn <loop.infinitely@gmail.com>
2024-04-13 16:24:58 +0900

    feat(lab3.control_unit_def): add definitions for control unit states

--- lab3/control_unit_def.v
+++ lab3/control_unit_def.v
@@ -1,3 +1,9 @@
-`define CTRL_OP2_REG 0
-`define CTRL_OP2_FOUR 1
-`define CTRL_OP2_IMM 2
+`define CTRL_OP2_REG 2'd0
+`define CTRL_OP2_FOUR 2'd1
+`define CTRL_OP2_IMM 2'd2
+
+`define CTRL_IF_STAGE 3'd0
+`define CTRL_ID_STAGE 3'd1
+`define CTRL_EX_STAGE 3'd2
+`define CTRL_MEM_STAGE 3'd3
+`define CTRL_WB_STAGE 3'd4
