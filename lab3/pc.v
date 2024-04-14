d4222b8223966a24de7cfc0c6dbcc1920b118cab
Ryang Sohn <loop.infinitely@gmail.com>
2024-04-14 16:06:30 +0900

    feat(lab3.pc): add PC commit signal

--- lab3/pc.v
+++ lab3/pc.v
@@ -3,11 +3,19 @@ module pc (
     input clk,
     input [31:0] next_pc,
     input pc_write,
+    input pc_commit,
 
     output reg [31:0] current_pc
 );
+  reg [31:0] next_pc_temp;
+
   always @(posedge clk) begin
-    if (reset) current_pc <= 32'b0;
-    else if (pc_write) current_pc <= next_pc;
+    if (reset) begin
+      current_pc   <= 32'b0;
+      next_pc_temp <= 32'b0;
+    end else if (pc_write) begin
+      next_pc_temp <= next_pc;
+      if (pc_commit) current_pc <= next_pc;
+    end else if (pc_commit) current_pc <= next_pc_temp;
   end
 endmodule
