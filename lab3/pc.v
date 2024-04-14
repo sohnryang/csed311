29049c00b28e06b86d8e178af65d2cc67e63ee2d
Ryang Sohn <loop.infinitely@gmail.com>
2024-04-14 13:15:46 +0900

    feat(lab3.pc): add write enable pin to PC module

--- lab3/pc.v
+++ lab3/pc.v
@@ -2,11 +2,12 @@ module pc (
     input reset,
     input clk,
     input [31:0] next_pc,
+    input pc_write,
 
     output reg [31:0] current_pc
 );
   always @(posedge clk) begin
     if (reset) current_pc <= 32'b0;
-    else current_pc <= next_pc;
+    else if (pc_write) current_pc <= next_pc;
   end
 endmodule
