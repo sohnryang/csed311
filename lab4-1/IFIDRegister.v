00b263ac1884462c0bd7000fe04c555e4143b41b
Ryang Sohn <loop.infinitely@gmail.com>
2024-04-16 19:35:16 +0900

    feat(lab4-1.IFIDRegister): add IF/ID pipeline register

---
+++ lab4-1/IFIDRegister.v
@@ -0,0 +1,14 @@
+module IFIDRegister (
+    input clk,
+    input reset,
+
+    input inst_reg_write,
+    input [31:0] inst_in,
+
+    output reg [31:0] inst_out
+);
+  always @(posedge clk) begin
+    if (reset) inst_out <= 32'b0;
+    else if (inst_reg_write) inst_out <= inst_in;
+  end
+endmodule
