10ccbc8f5bae645685da8012ec9eb5c597f99782
Aplace <aplace030927@gmail.com>
2024-04-30 20:53:11 +0900

    feat(lab4-2.BranchPrd): Branch predictor design concepts

--- lab4/BranchPredictor.v
+++ lab4/BranchPredictor.v
@@ -1,6 +1,32 @@
+`include "branchpredictor_def.v"
+
 module BranchPredictor (
     input  [31:0] current_pc,
-    output [31:0] predicted_pc
+    output [31:0] predicted_pc,
+
+    input update_pred,          // When update
+    input actually_taken,       // Actually taken data
+    input [31:0] taken_address  // Actually taken addr
 );
-  assign predicted_pc = current_pc + 4;
+  // ---------- Global Branch History Register ----------
+  reg [`BHSR_WIDTH - 1:0] bhsr;       // 10-bit BHSR 
+  // ---------- Pattern History Table ----------
+  reg [`BHSR_ENTRIES - 1:0] pht[1:0];
+  // ---------- Branch Target Buffer ----------
+  reg [`BHSR_ENTRIES - 1:0] btb_tag_tbl[31 - `BHSR_WIDTH: 0];
+  reg [`BHSR_ENTRIES - 1:0] btb_target_tbl[31:0];
+
+  wire [`BHSR_WIDTH - 1:0] pc_lsb;    // LSB of current PC
+  wire [31 - `BHSR_WIDTH:0] pc_msb;   // MSB of current PC
+  wire [`BHSR_WIDTH - 1:0] pht_index; // XORed PC with BHSR of 
+
+  assign pc_msb = current_pc[31 : `BHSR_WIDTH];
+  assign pc_lsb = current_pc[`BHSR_WIDTH - 1: 0];
+  assign pht_index = pc_lsb ^ bhsr;
+
+  if ((pht[pht_index] >= `WEAK_NOT_TAKEN) && (btb_tag_tbl[pc_lsb] == msb_pc)) begin // Tag matches, Likely to take branch
+    assign predicted_pc = btb_target_tbl[pc_lsb];
+  end else begin
+    assign predicted_pc = current_pc + 4;
+  end
 endmodule
---
+++ lab4/branchpredictor_def.v
@@ -0,0 +1,7 @@
+`define BHSR_WIDTH 10
+`define BHSR_ENTRIES (1 << BHSR_WIDTH)
+
+`define STRONG_NOT_TAKEN 2'b00
+`define WEAK_NOT_TAKEN 2'b01
+`define WEAK_TAKEN 2'b10
+`define STRONG_TAKEN 2'b11
