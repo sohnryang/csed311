f72d92a2ec51e131216067579e2d519b878f035e
Ryang Sohn <loop.infinitely@gmail.com>
2024-05-07 20:18:15 +0900

    fix(lab4-2.PCGenerator): use rd_to_pc signal in PC generator

--- lab4/PCGenerator.v
+++ lab4/PCGenerator.v
@@ -1,14 +1,15 @@
 module PCGenerator (
     input is_branch,
-    input is_jalr,
+    input is_rd_to_pc,
     input [31:0] current_pc,
     input [31:0] rs1_data,
     input [31:0] imm,
+    input [31:0] alu_result,
 
     output reg [31:0] next_pc
 );
   always @(*) begin
-    if (is_jalr) next_pc = rs1_data + imm;
+    if (is_rd_to_pc) next_pc = alu_result;
     else if (is_branch) next_pc = current_pc + imm;
     else next_pc = current_pc + 4;
   end
