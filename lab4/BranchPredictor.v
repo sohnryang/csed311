22c76986d71a8521d7908b99d8546e347a6af3e5
Aplace <aplace030927@gmail.com>
2024-05-12 19:51:26 +0900

    fix(lab4-2.BranchPrd): prediction index

--- lab4/BranchPredictor.v
+++ lab4/BranchPredictor.v
@@ -39,9 +39,11 @@ module BranchPredictor (
   assign pht_index = pc_lsb ^ bhsr;
 
   always @(*) begin
-    if ((pht[pht_index] >= `WEAK_NOT_TAKEN) && (btb_tag_tbl[pc_lsb] == pc_msb)) begin // Tag matches, Likely to take branch
+    if ((pht[pht_index] >= `WEAK_TAKEN) && (btb_tag_tbl[pc_lsb] == pc_msb)) begin // Tag matches, Likely to take branch
+      $display("Predict  T to %x -> %x (%x), because PHT[%x] = %x with HISTORY[%b] and TAG MATCH [%x = %x]", current_pc, btb_target_tbl[pc_lsb], pc_lsb, pht_index, pht[pht_index], bhsr, btb_tag_tbl[pc_lsb], pc_msb);
       assign predicted_pc = btb_target_tbl[pc_lsb];
     end else begin
+      $display("Predict NT to %x -> %x (%x), because PHT[%x] = %x with HISTORY[%b] and TAG MATCH [%x = %x]", current_pc, current_pc + 4, pc_lsb, pht_index, pht[pht_index], bhsr, btb_tag_tbl[pc_lsb], pc_msb);
       assign predicted_pc = current_pc + 4;
     end
   end
@@ -49,44 +51,51 @@ module BranchPredictor (
 
   // ---------- Sync. Branch Predictor state update ----------
   integer i;
-  always @(posedge clk && update_pred) begin
+  always @(posedge clk) begin
     if (rst) begin
-      bhsr <= 0;
+      bhsr <= `BHSR_WIDTH'b0;
       for (i = 0; i < `BHSR_ENTRIES; i = i + 1) begin
         pht[i] <= 2'b00;
         btb_tag_tbl[i] <= 0;
         btb_target_tbl[i] <= 0;
       end
     end
-    update_pc_msb <= branch_inst_address[31:`BHSR_WIDTH];
-    update_pc_lsb <= branch_inst_address[`BHSR_WIDTH-1:0];
-    update_pht_index <= update_pc_lsb ^ bhsr;
+
+    if (update_pred && predictor_wrong) begin
+      update_pc_msb <= branch_inst_address[31:`BHSR_WIDTH];
+      update_pc_lsb <= branch_inst_address[`BHSR_WIDTH-1:0];
+      update_pht_index <= update_pc_lsb ^ bhsr;
 
 
-    /* Updating 2-bit saturatrion counter FSM state */
-    if (predictor_wrong == 1'b0) begin  // Predictor Correct
-      if (branch_inst_address + 4 == resolved_next_pc) begin  // Pred: NT, Actual: NT
-        pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_NOT_TAKEN ? `STRONG_NOT_TAKEN : pht[update_pht_index] - 1); // Decrease counter
-        btb_tag_tbl[update_pht_index] <= update_pc_msb;
-        btb_target_tbl[update_pht_index] <= resolved_next_pc;
-        bhsr <= {bhsr[`BHSR_WIDTH-1:1], 1'b0};
-      end else begin  // Pred: T, Actual: T
-        pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_TAKEN ? `STRONG_TAKEN : pht[update_pht_index] + 1); // Increase Counter
-        btb_tag_tbl[update_pht_index] <= update_pc_msb;
-        btb_target_tbl[update_pht_index] <= resolved_next_pc;
-        bhsr <= {bhsr[`BHSR_WIDTH-1:1], 1'b1};
-      end
-    end else begin  // Predictor Wrong
-      if (branch_inst_address + 4 == resolved_next_pc) begin  // Predict: NT, Actual: T
-        pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_TAKEN ? `STRONG_TAKEN : pht[update_pht_index] + 1); // Increase Counter
-        btb_tag_tbl[update_pht_index] <= update_pc_msb;
-        btb_target_tbl[update_pht_index] <= resolved_next_pc;
-        bhsr <= {bhsr[`BHSR_WIDTH-1:1], 1'b1};
-      end else begin  // Pred: T, Actual: NT
-        pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_NOT_TAKEN ? `STRONG_NOT_TAKEN : pht[update_pht_index] - 1); // Decrease counter
-        btb_tag_tbl[update_pht_index] <= update_pc_msb;
-        btb_target_tbl[update_pht_index] <= resolved_next_pc;
-        bhsr <= {bhsr[`BHSR_WIDTH-1:1], 1'b0};
+      /* Updating 2-bit saturatrion counter FSM state */
+      if (predictor_wrong == 1'b1) begin  // Predictor Correct
+        if (predicted_pc + 4 == resolved_next_pc) begin  // Pred: NT, Actual: NT
+          pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_NOT_TAKEN ? `STRONG_NOT_TAKEN : pht[update_pht_index] - 1); // Decrease counter
+          btb_tag_tbl[update_pc_lsb] <= update_pc_msb;
+          btb_target_tbl[update_pc_lsb] <= resolved_next_pc;
+          bhsr <= (bhsr << 1 | `BHSR_WIDTH'b0);
+          $display("(%x -> %x) P NT == R NT, PHT[%x] = %x with HISTORY = [%b] and TARGET[%x] = %x",predicted_pc, resolved_next_pc, update_pht_index, pht[update_pht_index], bhsr, update_pc_lsb, btb_target_tbl[update_pc_lsb]);
+        end else begin  // Pred: T, Actual: T
+          pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_TAKEN ? `STRONG_TAKEN : pht[update_pht_index] + 1); // Increase Counter
+          btb_tag_tbl[update_pc_lsb] <= update_pc_msb;
+          btb_target_tbl[update_pc_lsb] <= resolved_next_pc;
+          bhsr <= (bhsr << 1 | `BHSR_WIDTH'b1);
+          $display("(%x -> %x) P  T == R  T, PHT[%x] = %x with HISTORY = [%b] and TARGET[%x] = %x",predicted_pc, resolved_next_pc, update_pht_index, pht[update_pht_index], bhsr, update_pc_lsb, btb_target_tbl[update_pc_lsb]);
+        end
+      end else begin  // Predictor Wrong
+        if (predicted_pc + 4 == resolved_next_pc) begin  // Predict: NT, Actual: T
+          pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_TAKEN ? `STRONG_TAKEN : pht[update_pht_index] + 1); // Increase Counter
+          btb_tag_tbl[update_pc_lsb] <= update_pc_msb;
+          btb_target_tbl[update_pc_lsb] <= resolved_next_pc;
+          bhsr <= (bhsr << 1 | `BHSR_WIDTH'b1);
+          $display("(%x -> %x) P NT != R  T, PHT[%x] = %x with HISTORY = [%b] and TARGET[%x] = %x",predicted_pc, resolved_next_pc, update_pht_index, pht[update_pht_index], bhsr, update_pc_lsb, btb_target_tbl[update_pc_lsb]);
+        end else begin  // Pred: T, Actual: NT
+          pht[update_pht_index] <= (pht[update_pht_index] == `STRONG_NOT_TAKEN ? `STRONG_NOT_TAKEN : pht[update_pht_index] - 1); // Decrease counter
+          btb_tag_tbl[update_pc_lsb] <= update_pc_msb;
+          btb_target_tbl[update_pc_lsb] <= resolved_next_pc;
+          bhsr <= (bhsr << 1 | `BHSR_WIDTH'b0);
+          $display("(%x -> %x) P  T != R NT, PHT[%x] = %x with HISTORY = [%b] and TARGET[%x] = %x",predicted_pc, resolved_next_pc, update_pht_index, pht[update_pht_index], bhsr, update_pc_lsb, btb_target_tbl[update_pc_lsb]);
+        end
       end
     end
   end
