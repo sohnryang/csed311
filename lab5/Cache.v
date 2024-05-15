b8d4b59def02329a506253f69ed4863be4430413
Aplace <aplace030927@gmail.com>
2024-05-15 21:48:36 +0900

    chore(lab5): ta modified code :ta_ggang:

--- lab5/Cache.v
+++ lab5/Cache.v
@@ -1,47 +1,38 @@
-module Cache #(
-    parameter LINE_SIZE = 16,
-    parameter NUM_SETS  = 0,
-    parameter NUM_WAYS  = 0
-) (
+module Cache #(parameter LINE_SIZE = 16,
+               parameter NUM_SETS = /* Your choice */
+               parameter NUM_WAYS = /* Your choice */) (
     input reset,
     input clk,
 
     input is_input_valid,
     input [31:0] addr,
-    input mem_read,
-    input mem_write,
+    input mem_rw,
     input [31:0] din,
 
     output is_ready,
     output is_output_valid,
     output [31:0] dout,
-    output is_hit
-);
+    output is_hit);
   // Wire declarations
   wire is_data_mem_ready;
   // Reg declarations
   // You might need registers to keep the status.
 
-  assign is_ready = is_data_mem_ready;
-  assign is_hit   = 0;
-
   // Instantiate data memory
-  DataMemory #(
-      .BLOCK_SIZE(LINE_SIZE)
-  ) data_mem (
-      .reset(reset),
-      .clk  (clk),
+  DataMemory #(.BLOCK_SIZE(LINE_SIZE)) data_mem(
+    .reset(reset),
+    .clk(clk),
 
-      .is_input_valid(is_input_valid),
-      .addr(addr << `CLOG(LINE_SIZE)),  // NOTE: address must be shifted by CLOG2(LINE_SIZE)
-      .mem_read(mem_read),
-      .mem_write(mem_write),
-      .din(din),
+    .is_input_valid(is_input_valid),
+    .addr(addr << `CLOG2(LINE_SIZE)),        // NOTE: address must be shifted by CLOG2(LINE_SIZE)
+    .mem_read(),
+    .mem_write(),
+    .din(din),
 
-      // is output from the data memory valid?
-      .is_output_valid(is_output_valid),
-      .dout(dout),
-      // is data memory ready to accept request?
-      .mem_ready(is_data_mem_ready)
+    // is output from the data memory valid?
+    .is_output_valid(is_output_valid),
+    .dout(dout),
+    // is data memory ready to accept request?
+    .mem_ready(is_data_mem_ready)
   );
 endmodule
