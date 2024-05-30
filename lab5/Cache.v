b9a03ec407d853bd76e39f6b12ba50e3c1346a1c
Aplace <aplace030927@gmail.com>
2024-05-30 16:46:51 +0900

    feat(lab5): parametrize cache
    For ease of changing # of sets and index bits, (Almost of) definition
    had parametrized.
    
    * How to use
    
      - Change the `CACHE_SET_BITS` and `CACHE_WAY_BITS` in `cache_def.v`
        - Default value is (2, 2), and sum of both constant must be 4.
    
      - Change the `is_hit` in `Cache.v` (line 75~84)
        - Comment/Uncomment the `or` operations with proper cache
          definition.

--- lab5/Cache.v
+++ lab5/Cache.v
@@ -41,13 +41,13 @@ module Cache #(
       .mem_ready(data_mem_ready)
   );
 
-  reg [25:0] tag_of_addr;
-  reg [1:0] idx_of_addr;
+  reg [`CACHE_TAG_BITS - 1:0] tag_of_addr;
+  reg [`CACHE_IDX_BITS - 1:0] idx_of_addr;
   reg [31:0] data_mem_addr;
 
-  reg [(`CLOG2((LINE_SIZE >> 2))) - 1:0] block_offset_of_addr;
+  reg [`CACHE_BLO_BITS - 1:0] block_offset_of_addr;
   reg [1:0] state, next_state;
-  reg [`CACHE_IDX_BITS - 1:0] stored_set;
+  reg [`CACHE_WAY_BITS - 1:0] stored_set;
   
   reg [2:0] cache_control;
 
@@ -61,9 +61,9 @@ module Cache #(
   reg [LINE_SIZE * 8 - 1: 0]       data_table [0: `CACHE_SET_COUNT - 1][0: `CACHE_WAY_COUNT - 1];
   reg                             valid_table [0: `CACHE_SET_COUNT - 1][0: `CACHE_WAY_COUNT - 1];
   reg                             dirty_table [0: `CACHE_SET_COUNT - 1][0: `CACHE_WAY_COUNT - 1];
-  reg [`CACHE_WAY_COUNT - 1: 0]     lru_table [0: `CACHE_SET_COUNT - 1];
+  reg [`CACHE_SET_COUNT - 1: 0]     lru_table [0: `CACHE_SET_COUNT - 1];
 
-  reg [1: 0] victim;
+  reg [`CACHE_WAY_BITS - 1: 0] victim;
 
   assign is_ready = (next_state == `STATE_CACHE_STANDBY);
   assign is_output_valid = (state == `STATE_CACHE_READ);
@@ -72,20 +72,27 @@ module Cache #(
   assign idx_of_addr = addr[`CACHE_IDX_FIELD];
   assign block_offset_of_addr = addr[`CACHE_BLO_FIELD];    
 
-  assign stored_set = (
-    (tag_table[idx_of_addr][0] == tag_of_addr) && (valid_table[idx_of_addr][0] == 1'b1) ? 0: ( 
-    (tag_table[idx_of_addr][1] == tag_of_addr) && (valid_table[idx_of_addr][1] == 1'b1) ? 1: (
-    (tag_table[idx_of_addr][2] == tag_of_addr) && (valid_table[idx_of_addr][2] == 1'b1) ? 2: (
-    (tag_table[idx_of_addr][3] == tag_of_addr) && (valid_table[idx_of_addr][3] == 1'b1) ? 3: 0)))
+  assign is_hit = (
+    ((tag_table[idx_of_addr][0] == tag_of_addr) && (valid_table[idx_of_addr][0] == 1'b1)) ||    
+    ((tag_table[idx_of_addr][1] == tag_of_addr) && (valid_table[idx_of_addr][1] == 1'b1)) ||    // |Cache set| = 3
+    ((tag_table[idx_of_addr][2] == tag_of_addr) && (valid_table[idx_of_addr][2] == 1'b1)) ||
+    ((tag_table[idx_of_addr][3] == tag_of_addr) && (valid_table[idx_of_addr][3] == 1'b1)) //||    // |Cache set| = 2
+    // ((tag_table[idx_of_addr][4] == tag_of_addr) && (valid_table[idx_of_addr][4] == 1'b1)) ||
+    // ((tag_table[idx_of_addr][5] == tag_of_addr) && (valid_table[idx_of_addr][5] == 1'b1)) ||
+    // ((tag_table[idx_of_addr][6] == tag_of_addr) && (valid_table[idx_of_addr][6] == 1'b1)) ||
+    // ((tag_table[idx_of_addr][7] == tag_of_addr) && (valid_table[idx_of_addr][7] == 1'b1))       // |Cache set| = 1
   );
 
-  assign is_hit = (
-    ((tag_table[idx_of_addr][0] == tag_of_addr) && (valid_table[idx_of_addr][0] == 1'b1)) ||
-    ((tag_table[idx_of_addr][1] == tag_of_addr) && (valid_table[idx_of_addr][1] == 1'b1)) ||
-    ((tag_table[idx_of_addr][2] == tag_of_addr) && (valid_table[idx_of_addr][2] == 1'b1)) ||
-    ((tag_table[idx_of_addr][3] == tag_of_addr) && (valid_table[idx_of_addr][3] == 1'b1))
-  );
-  
+  always @(*) begin
+    integer b;
+    stored_set = 0;
+    for (b = 0; b <= `CACHE_SET_COUNT - 1; b = b + 1) begin
+      if ((tag_table[idx_of_addr][b] == tag_of_addr) && (valid_table[idx_of_addr][b] == 1'b1)) begin
+        stored_set = `CACHE_WAY_BITS'(b);
+      end
+    end
+  end
+
   always @(*) begin
     case (state)
       `STATE_CACHE_STANDBY: begin
@@ -126,21 +133,7 @@ module Cache #(
   always @(*) begin
     data_mem_is_input_valid = 0;
     cache_control = 0;
-    
-    case (block_offset_of_addr)
-      2'b00: begin
-        dout = data_table[idx_of_addr][stored_set][31:0];
-      end
-      2'b01: begin
-        dout = data_table[idx_of_addr][stored_set][63:32];
-      end
-      2'b10: begin
-        dout = data_table[idx_of_addr][stored_set][95:64];
-      end
-      2'b11: begin
-        dout = data_table[idx_of_addr][stored_set][127:96];
-      end
-    endcase
+    dout = data_table[idx_of_addr][stored_set][block_offset_of_addr * 32 + 31 -: 32];
 
     case (state)
       `STATE_CACHE_STANDBY: begin
@@ -212,18 +205,19 @@ module Cache #(
 
   integer i;
   integer j;
+  integer b;
   always @(posedge clk) begin
     if (reset) begin
       state <= `STATE_CACHE_STANDBY;
       /* verilator lint_off BLKSEQ */
       for (i = 0; i < `CACHE_SET_COUNT; i = i + 1) begin
         for (j = 0; j < `CACHE_WAY_COUNT; j = j + 1) begin
-          tag_table[i][j] = `CACHE_TAG_BITS'b0;
+          tag_table[i][j] = {(`CACHE_TAG_BITS) {1'b0}};
           data_table[i][j] = {(LINE_SIZE * 8) {1'b0}};
           valid_table[i][j] = 1'b0;
           dirty_table[i][j] = 1'b0;
         end
-        lru_table[i] = `CACHE_WAY_COUNT'b0;
+        lru_table[i] = {(`CACHE_SET_COUNT) {1'b0}};
       end
       /* verilator lint_on BLKSEQ */
     end else begin
@@ -232,40 +226,21 @@ module Cache #(
       // ---------- Bit-PLRU (Pseudo LRU) ----------
       if (is_hit) begin
         if ((tag_table[idx_of_addr][stored_set] == tag_of_addr) && (valid_table[idx_of_addr][stored_set] == 1'b1)) begin // Cache Hit
-          if ((lru_table[idx_of_addr] | (1 << stored_set)) == 4'b1111) begin  // Filled with 1?
+          if ((lru_table[idx_of_addr] | (1 << stored_set)) == {`CACHE_SET_COUNT {1'b1}}) begin  // Filled with 1?
             lru_table[idx_of_addr] <= (1 << stored_set); // Set everything except hit index as 0
           end else
             lru_table[idx_of_addr] <= (lru_table[idx_of_addr] | (1 << stored_set)); // Assign hit on the bit
         end
       end else begin
-        if ((lru_table[idx_of_addr] & (1 << 0)) == 4'b0) begin
-          victim <= 2'b00;
-        end else if ((lru_table[idx_of_addr] & (1 << 1)) == 4'b0) begin
-          victim <= 2'b01;
-        end else if ((lru_table[idx_of_addr] & (1 << 2)) == 4'b0) begin
-          victim <= 2'b10;
-        end else if ((lru_table[idx_of_addr] & (1 << 3)) == 4'b0) begin
-          victim <= 2'b11;
-        end else begin
-          victim <= 2'b00;
+        for (b = 0; b <= `CACHE_SET_COUNT - 1; b = b + 1) begin
+          if ((lru_table[idx_of_addr] & (1 << b)) == {`CACHE_SET_COUNT {1'b0}}) begin
+            victim <= `CACHE_WAY_BITS'(b);
+          end
         end
       end
 
       if ((cache_control & `ACTION_WRITE_CACHE) != 0) begin
-        case (block_offset_of_addr)
-          2'b00: begin
-            data_table[idx_of_addr][stored_set][31:0] <= din;
-          end
-          2'b01: begin
-            data_table[idx_of_addr][stored_set][63:32] <= din;
-          end
-          2'b10: begin
-            data_table[idx_of_addr][stored_set][95:64] <= din;
-          end
-          2'b11: begin
-            data_table[idx_of_addr][stored_set][127:96] <= din;
-          end
-        endcase
+        data_table[idx_of_addr][stored_set][block_offset_of_addr * 32 + 31 -: 32] <= din;
         dirty_table[idx_of_addr][stored_set] <= 1'b1;
       end
 
